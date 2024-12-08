5665b0d8d21097796c21425014b354e5133e3b78
samarth <samarthkulkarni@icloud.com>
2024-12-09 03:10:02 +0530

    logic written for antinode pair locs

---
+++ aoc_8/src/main.rs
@@ -0,0 +1,6 @@
+/*
+antinode_pair_locs = (antenna_1 + slope, antenna_2 - slope) where,
+                                                                slope = |antenna_1 - antenna|
+                                                              & antenna_1.y > antenna_2.y
+                                                                (i.e. antenna_1 is below antenna_2 in the 2d map)
+*/
