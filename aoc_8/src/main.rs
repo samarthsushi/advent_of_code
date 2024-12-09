8a115a3d2c5019b356d79017571f2b086fe42cd1
samarth <samarthkulkarni@icloud.com>
2024-12-09 12:28:16 +0530

    6-2

--- aoc_8/src/main.rs
+++ aoc_8/src/main.rs
@@ -44,15 +44,26 @@ impl Map {
 			for i in 0..loc_t.len() {
 				for j in 0..loc_t.len() {
 					if i==j { continue; }
-					let my = loc_t[i].0 - loc_t[j].0;
-					let mx = loc_t[i].1 - loc_t[j].1;
-					let antinode1y = loc_t[i].0 + my;
-					let antinode1x = loc_t[i].1 + mx;
+					let dy = loc_t[i].0 - loc_t[j].0;
+					let dx = loc_t[i].1 - loc_t[j].1;
+
+					if dx % 3 == 0 && dy % 3 == 0 {
+						let internal_a1y = loc_t[i].0 + (dy/3);
+						let internal_a1x = loc_t[i].1 + (dx/3);
+						self.grid[internal_a1y as usize][internal_a1x as usize] = CellStatus::Antinode;
+
+						let internal_a2y = loc_t[i].0 + (2*dy/3);
+						let internal_a2x = loc_t[i].1 + (2*dx/3);
+						self.grid[internal_a2y as usize][internal_a2x as usize] = CellStatus::Antinode;
+					}
+
+					let antinode1y = loc_t[i].0 + dy;
+					let antinode1x = loc_t[i].1 + dx;
 					if (0..50).contains(&antinode1x) && (0..50).contains(&antinode1y) {
 						self.grid[antinode1y as usize][antinode1x as usize] = CellStatus::Antinode;
 					}
-					let antinode2y = loc_t[j].0 - my;
-					let antinode2x = loc_t[j].1 - mx;
+					let antinode2y = loc_t[j].0 - dy;
+					let antinode2x = loc_t[j].1 - dx;
 					if (0..50).contains(&antinode2x) && (0..50).contains(&antinode2y) {
 						self.grid[antinode2y as usize][antinode2x as usize] = CellStatus::Antinode;
 					}
@@ -62,10 +73,54 @@ impl Map {
 		let mut antinodes = 0;
 		for row in self.grid {
 			for col in row {
-				if CellStatus::Antinode == col { print!("#"); antinodes+=1; }
-				else { print!("."); }
+				if CellStatus::Antinode == col { antinodes+=1; }
 			}
-			println!();
+		}
+		antinodes
+	}
+
+	pub fn parttwo(&mut self) -> usize {
+		for (_, loc_t) in &self.antenna_clusters {
+			for i in 0..loc_t.len() {
+				for j in 0..loc_t.len() {
+					if i==j { continue; }
+					self.grid[loc_t[i].0 as usize][loc_t[i].1 as usize] = CellStatus::Antinode;
+					self.grid[loc_t[j].0 as usize][loc_t[j].1 as usize] = CellStatus::Antinode;
+					let dy = loc_t[i].0 - loc_t[j].0;
+					let dx = loc_t[i].1 - loc_t[j].1;
+
+					if dx % 3 == 0 && dy % 3 == 0 {
+						let internal_a1y = loc_t[i].0 + (dy/3);
+						let internal_a1x = loc_t[i].1 + (dx/3);
+						self.grid[internal_a1y as usize][internal_a1x as usize] = CellStatus::Antinode;
+
+						let internal_a2y = loc_t[i].0 + (2*dy/3);
+						let internal_a2x = loc_t[i].1 + (2*dx/3);
+						self.grid[internal_a2y as usize][internal_a2x as usize] = CellStatus::Antinode;
+					}
+
+					let mut antinode1y = loc_t[i].0 + dy;
+					let mut antinode1x = loc_t[i].1 + dx;
+					while (0..50).contains(&antinode1x) && (0..50).contains(&antinode1y) {
+						self.grid[antinode1y as usize][antinode1x as usize] = CellStatus::Antinode;
+						antinode1y+=dy;
+						antinode1x+=dx;
+					}
+					let mut antinode2y = loc_t[j].0 - dy;
+					let mut antinode2x = loc_t[j].1 - dx;
+					while (0..50).contains(&antinode2x) && (0..50).contains(&antinode2y) {
+						self.grid[antinode2y as usize][antinode2x as usize] = CellStatus::Antinode;
+						antinode2x+=dx;
+						antinode2y+=dy;
+					}
+				}
+			}
+		}
+		let mut antinodes = 0;
+		for row in self.grid {
+			for col in row {
+				if CellStatus::Antinode == col { antinodes+=1; }
+			}
 		}
 		antinodes
 	}
@@ -76,6 +131,7 @@ fn main() {
 	let s = std::fs::read_to_string(file_path).unwrap();
 
 	let mut map = Map::new(s);
-	let x = map.partone();
-	println!("{x}");
+	let p1 = map.partone();
+	let p2 = map.parttwo();
+	println!("{p1}\n{p2}");
 }
